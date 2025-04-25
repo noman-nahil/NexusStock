5a67670b7acc82488027d547c13d0c88fc076147
noman-nahil <abdullahnoman1997@gmail.com>
2025-04-25 18:23:28 +0600

    Add route to get product by ID

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -9,6 +9,20 @@ const productList = (req, res) => {
   });
 };
 
+const getProductDetails = (req, res) => {
+  const id = parseInt(req.params.id);
+  db.getProductData().then((products) => {
+    const product = products.find((product) => product.id === id);
+    if (!product) {
+      res.status(400).send("Not Found");
+    } else {
+      res.send(product);
+    }
+  });
+};
+
 router.route("/").get(productList);
+
+router.route("/:id").get(getProductDetails);
 
 module.exports = router;
